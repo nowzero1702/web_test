31988acd2e12d8f4648fb2aa6c56be5eb07f7619
nowzero1702 <imkorea12@naver.com>
2024-11-20 00:41:21 +0900

    Update home.js

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const User = require('C:\\Users\\nowzero\\web_test\\src\\models\\User.js'); // 모델 경로 확인
+const User = require('..\\models\\User.js'); // 모델 경로 확인
 const router = express.Router();
 
 // 기본 라우트
@@ -18,5 +18,20 @@ router.get('/create-user', async (req, res) => {
         res.status(500).send('사용자 생성 중 오류 발생');
     }
 });
+
+router.post('/add-user', async (req, res) => {
+    try {
+        const newUser = new User({
+            name: req.body.name,
+            email: req.body.email,
+            age: req.body.age,
+        });
+        await newUser.save();
+        res.status(201).send('사용자 추가 성공');
+    } catch (err) {
+        res.status(500).send('사용자 추가 실패: ' + err.message);
+    }
+});
+
 
 module.exports = router;
